284a608299b30f5c314567ad0be70a9955d3a498
Jordan <rectorjordan94@gmail.com>
2024-04-02 11:32:34 -0700

    finalizes solutions in app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const data = [
     { name: "Jem", address: "Starlight Music" }
 ];
 
-document.addEventListener('DOMContentLoaded', () => {
+const populateData = () => {
     for (let i = 0; i < data.length; i++) {
         // create info container to hold all the divs
         const infoContainer = document.createElement('div')
@@ -33,4 +33,46 @@ document.addEventListener('DOMContentLoaded', () => {
         // append the info container to the body of the document
         document.querySelector('body').appendChild(infoContainer)
     }
+}
+
+// write a function that will push new data in to the array
+// the function should take name and address as parameters
+// the function should then run the populateData function
+const addData = (name, address) => {
+    data.push({ name, address })
+    // clear out the old information before populating with the new info added
+    const body = document.querySelector('body')
+    while (body.firstChild) {
+        body.removeChild(body.firstChild)
+    }
+    populateData()
+}
+
+// create a function that takes the name of a person you want to remove, removes them from the data array, then refreshes the rolodex
+const removeData = (name) => {
+    // access the obj you wish to remove by its name value
+    // iterate over the array to accomplish?
+    for (let i = 0; i < data.length; i++) {
+        // console.log(data[i].name)
+        if (data[i].name === name) {
+            // console.log(data[i])
+            // remove the entire object from the array
+            data.splice(i, 1)
+        }
+    }
+    // refresh the rolodex
+    const body = document.querySelector('body')
+    while (body.firstChild) {
+        body.removeChild(body.firstChild)
+    }
+    populateData();
+}
+
+// console.log(data)
+
+document.addEventListener('DOMContentLoaded', () => {
+    populateData()
+    addData('Taylor', 'Seattle')
+    removeData("Grace Hopper")
+    // removeData('Umbra')
 })
